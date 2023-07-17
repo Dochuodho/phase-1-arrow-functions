910cb609ff91d72edbdcfc70b32218c83cfd4588
Ochuodho <ochuodhoderrick@gmail.com>
2023-07-17 19:52:14 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,21 @@
+const divide = () =>{
+    let num1=2000;
+    let num2=100;
+    let result=num1/num2;
+    console.log(result)
+    return result;
+
+}
+divide();
+
+const square =(x)=>{
+    return x*x;
+
+}
+square(2);
+
+const add = (a, b) =>{
+    return a+b;
+
+}
+add(3, 4);
